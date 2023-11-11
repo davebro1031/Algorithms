2b34773660bb42a3a386e6ef806601470d7191de
davebro1031 <dwbroaddus@gmail.com>
2023-11-10 23:31:53 -0700

    test merge

--- mergeSort.js
+++ mergeSort.js
@@ -3,6 +3,8 @@ const merge = (arr1, arr2) => {
     while(arr1.length && arr2.length){
         merged.push( arr1[0] < arr2[0] ? arr1.shift() : arr2.shift())
     }
-
-    return merged
+    merged.push(arr1.length ? arr1 : arr2)
+    return merged.flat()
 }
+
+console.log(merge([1,1,3,7,9,11], [1,2,3,4,6,8,10,11,12,13,14]))
