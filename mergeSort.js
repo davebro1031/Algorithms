f773a5dc3ac7ff570d56fd03f4e616c91878e25e
davebro1031 <dwbroaddus@gmail.com>
2023-11-11 08:28:00 -0700

    complete mergeSort algorithm

--- mergeSort.js
+++ mergeSort.js
@@ -5,4 +5,19 @@ const merge = (arr1, arr2) => {
     return merged
 }
 
+const split = (arr) => {
+    const splitPoint = Math.floor(arr.length / 2)
+    const arr1 = arr.slice(0, splitPoint)
+    const arr2 = arr.slice(splitPoint)
+    console.log(splitPoint)
+    return [arr1, arr2]
+}
+
+const mergeSort = (arr) => {
+    if (arr.length === 1) return arr
+    let [arr1, arr2] = split(arr)
+    return merge(mergeSort(arr1), mergeSort(arr2))
+}
+
 console.log(merge([1, 1, 2, 2, 3, 3, 4, 5, 8, 10, 11, 12, 13, 13], [1, 1, 2, 3, 6, 9, 10]))
+console.log(mergeSort([3, 6, 2, 8, 1, 1, 6, 9, 12, 3, 2, 10, 0, 11]))
