3309f20288b7f5419279cdb0cc042f03c857a21f
davebro1031 <dwbroaddus@gmail.com>
2023-11-10 23:36:42 -0700

    refactor merge

--- mergeSort.js
+++ mergeSort.js
@@ -1,9 +1,9 @@
 const merge = (arr1, arr2) => {
-    const merged = []
+    let merged = []
     while(arr1.length && arr2.length)merged.push( arr1[0] < arr2[0] ? arr1.shift() : arr2.shift())
-    merged.push(...arr1)
-    merged.push(...arr2)
+    merged = [...merged, ...arr1, ...arr2]
+    // merged.push(...arr2)
     return merged
 }
 
-console.log(merge([1,1,2,3,6,9,10], [1,1,2,2,3,3,4,5,8,10,11,12,13,13]))
+console.log(merge([1,1,2,2,3,3,4,5,8,10,11,12,13,13], [1,1,2,3,6,9,10]))
