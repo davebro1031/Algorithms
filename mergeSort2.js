23802db332dfa44bc407707d807856026dbbbc94
davebro1031 <dwbroaddus@gmail.com>
2023-11-12 09:12:58 -0700

    complete merge function

--- mergeSort2.js
+++ mergeSort2.js
@@ -5,13 +5,20 @@ const merge = (A, p, q, r) => {
     let R = []
     for (let i = 0; i < n1; i++) L[i] = A[p + i]
     for (let i = 0; i < n2; i++) R[i] = A[q + 1 + i]
-    L[n1] = 1/0
-    R[n2] = 1/0
-    
+    L[n1] = 1 / 0
+    R[n2] = 1 / 0
+
     let i = 0
     let j = 0
-    console.log(L, R)
-
+    for (k = p; k <= r; k++) {
+        if (L[i] < R[j]) {
+            A[k] = L[i++]
+        } else {
+            A[k] = R[j++]
+        }
+    }
 }
+let arr = [0, 1, 2, 3, 4, 5, 6, 1, 3, 5, 10, 11, 12, 13, 14, 15]
+merge(arr, 3, 6, 9)
 
-merge([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], 3,6,9)
+console.log(arr)
