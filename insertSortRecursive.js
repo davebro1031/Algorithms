d500c738ad82b2b6df7a74c40a4c2b4da6fedcd4
davebro1031 <dwbroaddus@gmail.com>
2023-11-14 20:39:13 -0700

    fix insert function

--- insertSortRecursive.js
+++ insertSortRecursive.js
@@ -1,6 +1,6 @@
 const insert = (key, arr) => {
     for(let i = 0; i < arr.length; i++){
-        if(key > arr[i]){
+        if(key < arr[i]){
             temp = arr[i]
             arr[i] = key
             key = temp
@@ -8,3 +8,8 @@ const insert = (key, arr) => {
     }
     arr[arr.length] = key
 }
+
+let arr = [1,1,2,3,5,8,13,21]
+let key = 20
+insert(key, arr)
+console.log(arr)
