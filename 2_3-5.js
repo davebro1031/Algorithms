dd88d566d38bc4c376f908a3418772df98224d27
davebro1031 <dwbroaddus@gmail.com>
2023-11-15 15:16:51 -0700

    init 2_3-5

--- 2_3-5.js
+++ 2_3-5.js
@@ -1,16 +1,3 @@
-const insert = (arr, n) => {
-    const key = arr[n]
-    let j = n - 1
-    while (key < arr[j]) {
-        arr[j + 1] = arr[j]
-        arr[j--] = key
-    }
-
-}
-
-const insertSortRecursive = (arr, n) => {
-    if (n > 1) {
-        insertSortRecursive(arr, n - 1)
-        insert(arr, n-1)
-    }
+const binarySearch = (arr, n) => {
+    
 }
