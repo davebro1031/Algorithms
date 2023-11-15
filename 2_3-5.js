bb07ba7e234eac6949d0f7e2b1ad0a427ee2b811
davebro1031 <dwbroaddus@gmail.com>
2023-11-15 15:35:28 -0700

    add binarySearch draft

--- 2_3-5.js
+++ 2_3-5.js
@@ -1,3 +1,11 @@
-const binarySearch = (arr, p, q, n) => {
-
+const binarySearch = (arr, p, r, n) => {
+    if(p >= r) return "not found"
+    const q = Math.floor((p + r) / 2)
+    if (arr[q] === n) return q
+    if (arr[q] < n) {
+        binarySearch(arr, p, q, n)
+    } else {
+        binarySearch(arr, q, r, n)
+    }
 }
+
