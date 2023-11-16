11a233829f5cbd2417a690d6f1144bf0b958fc48
davebro1031 <dwbroaddus@gmail.com>
2023-11-15 23:42:11 -0700

    add guard clause

--- 2_3-5.js
+++ 2_3-5.js
@@ -1,11 +1,13 @@
 const binarySearch = (arr, p, r, n) => {
-    if(p >= r) return "not found"
+    if (n < arr[0] || n > arr[r]) return console.log(-1)
+    if (p >= r) return console.log(-1)
     const q = Math.floor((p + r) / 2)
-    if (arr[q] === n) return q
-    if (arr[q] < n) {
+    if (arr[q] === n) return console.log(q)
+    if (arr[q] > n) {
         binarySearch(arr, p, q - 1, n)
     } else {
         binarySearch(arr, q + 1, r, n)
     }
 }
-
+const arr = [1, 2, 5, 7, 9, 11, 11, 12, 13, 14, 17]
+binarySearch(arr, 0, arr.length - 1, 15)
