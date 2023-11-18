c9f47bceeeb7cd1a1d51cb370d413c5c05c87ebb
davebro1031 <dwbroaddus@gmail.com>
2023-11-17 23:23:51 -0700

    add bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,3 +1,11 @@
 const bubbleSort = (arr) => {
-    
+    for(let i = 0; i < arr.length - 1; i++){
+        for(let j = arr.length - 1; j > i; j++){
+            if(arr[j] > arr[j - 1]){
+                let temp = arr[j]
+                arr[j] = arr[j-1]
+                arr[j-1] = temp
+            }
+        }
+    }
 }
