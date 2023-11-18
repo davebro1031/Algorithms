a9ddb2c365bdf59620418ba05d04df290e572823
davebro1031 <dwbroaddus@gmail.com>
2023-11-18 00:00:31 -0700

    add merge count

--- countInversions.js
+++ countInversions.js
@@ -1,5 +1,13 @@
-const countInversions = (arr, p, r) => {
+const merge = (arr, p, q, r, n) => {
+
+}
+
+const countInversions = (arr, p, r, n) => {
     if (p < r) {
         const q = Math.floor((p + r) / 2)
+        countInversions(arr, p, q, n)
+        countInversions(arr, q + 1, r, n)
+
     }
 }
+
