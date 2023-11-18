06afd83feecf7e02299dd016c2de4ccdb2065b06
davebro1031 <dwbroaddus@gmail.com>
2023-11-17 23:26:08 -0700

    debug bubblesort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,7 @@
 const bubbleSort = (arr) => {
     for(let i = 0; i < arr.length - 1; i++){
-        for(let j = arr.length - 1; j > i; j++){
-            if(arr[j] > arr[j - 1]){
+        for(let j = arr.length - 1; j > i; j--){
+            if(arr[j] < arr[j - 1]){
                 let temp = arr[j]
                 arr[j] = arr[j-1]
                 arr[j-1] = temp
@@ -9,3 +9,7 @@ const bubbleSort = (arr) => {
         }
     }
 }
+
+const arr = [2,3,4,5,3,2,7,4,6,4,2,6,9,8,4,3]
+bubbleSort(arr)
+console.log(arr)
