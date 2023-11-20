55f40916761207cde062ab58e611ba1bc87f47a2
davebro1031 <dwbroaddus@gmail.com>
2023-11-20 00:26:04 -0700

    fix cross finder

--- maximumSubarray.js
+++ maximumSubarray.js
@@ -1,28 +1,35 @@
 maximumSubarrayWithMid = (arr, p, q, r) => {
-    let minL = 0
+    let maxL = 0
+    let maxLI = q
+    let sumL = 0
     let maxR = 0
-    let minLI = p
     let maxRI = q
-    for (let i = p; i <= q; i++) {
-        if (minL >= arr[i]) {
-            minLI = i
-            minL = arr[i]
+    let sumR = 0
+    for (let i = q; i >= p; i--) {
+        sumL += arr[i]
+        if (maxL < sumL) {
+            maxLI = i
+            maxL = sumL
         }
     }
     for (let i = q; i <= r; i++) {
-        if (maxR < arr[i]) {
+        sumR += arr[i]
+        if (maxR < sumR) {
             maxRI = i
-            maxR = arr[i]
+            maxR = sumR
+            console.log(maxR)
         }
     }
-    return [minLI, maxRI, maxR - minL]
+    console.log(maxL, maxR)
+    return [maxLI, maxRI, maxR + maxL - arr[q]]
 }
 
 maximumSubarray = (arr, p, r) => {
-    if (p = r) {
+    console.log(p,r)
+    if (p === r) {
         return [p, r, arr[p]]
     } else {
-        const q = Math.max((p + r) / 2)
+        const q = Math.floor((p + r) / 2)
         const leftSum = maximumSubarray(arr, p, q)
         const rightSum = maximumSubarray(arr, q + 1, r)
         const midSum = maximumSubarrayWithMid(arr, p, q, r)
@@ -33,6 +40,6 @@ maximumSubarray = (arr, p, r) => {
     }
 }
 
-const arr = [-1,2,3,-2,-4,5,8,11,-10,-10,-1,3,4]
-const result = maximumSubarray(arr, 0, arr.length-1)
+const arr = [-1,2,3,-2,-4,5,8,-2,11,-10,-10,-1,3,4]
+const result = maximumSubarrayWithMid(arr, 0, Math.floor((arr.length-1)/2) ,arr.length-1)
 console.log(result)
