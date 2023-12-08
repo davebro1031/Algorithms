98f76bb8f1d9c1104a0e21f94d412a35d663ccfe
davebro1031 <dwbroaddus@gmail.com>
2023-12-07 21:46:45 -0700

    init slidingWindowMax

---
+++ C&C/slidingWindowMax.js
@@ -0,0 +1,11 @@
+
+const slidingWindowMax = (arr, W) => {
+    let result = []
+    
+
+}
+
+const arr = [6, 1, 2, 3, 1, 7, 2, 4, 5, 2]
+const W = 4
+const result = slidingWindowMax(arr, W)
+console.log(result)
