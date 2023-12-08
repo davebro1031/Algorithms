64a543193292dda38c7d4983d629c76101841350
davebro1031 <dwbroaddus@gmail.com>
2023-12-07 19:02:36 -0700

    add point space reducer

--- googlePogo.js
+++ googlePogo.js
@@ -1,9 +1,47 @@
-
 const findPath = (coordinates) => {
-    let [x, y] = coordinates.split(" ")
-    return x*y
+    let [x, y] = coordinates
+    if (x === 0 && y === 0) return ""
+    let dirs = ["N","E","S","W"]
+
+    // rotate point ccw into 1st quadrant
+    while (x < 0 || y <= 0) {
+        let temp = y
+        y = -x
+        x = temp
+
+        temp = dirs[3]
+        for(let i = 3; i > 0; i--){
+            dirs[i] = dirs[i-1]
+        }
+        dirs[0] = temp
+    }
+
+    // Reduce the work space further by considering only half of the 1st quadrant (y >= x)
+    if(x > y){
+        let temp = x
+        x = y
+        y = temp
+
+        temp = dirs[0]
+        dirs[0] = dirs[1]
+        dirs[1] = temp
+
+        temp = dirs[2]
+        dirs[2] = dirs[3]
+        dirs[3] = temp
+    }
+
+    // define distance from origin variable
+    let dist = x + y
+    let i = 1
+    let Ti = 1
+    while(dist > Ti || (Ti + dist)%2 === 1){
+        Ti += ++i
+    }
+    
+    return Ti
 }
 
-const coordinates = "3 -7"
+const coordinates = [7, -10]
 const result = findPath(coordinates)
 console.log(result)
