86943af5fb2d5da3a5d028f2523f494fabfaaee4
davebro1031 <dwbroaddus@gmail.com>
2023-12-08 01:23:44 -0700

    initialize stack

--- C&C/slidingWindowMax.js
+++ C&C/slidingWindowMax.js
@@ -1,64 +1,22 @@
 
 const slidingWindowMax = (arr, W) => {
-    // Initialize variables
-    let nextMax = {
-        val: -Infinity,
-        i: -1
-    }
-    let max = {
-        val: arr[0],
-        i: 0
-    }
+    // Initialize stack
+    let stack = [{ val: arr[0], i: 0 }]
     for (let i = 1; i < W; i++) {
-        if (arr[i] > max.val) {
-            max.val = arr[i]
-            max.i = i
-            nextMax.val = -1 / 0
-        } else if (arr[i] > nextMax.val) {
-            nextMax.val = arr[i]
-            nextMax.i = i
-        }
+        while (arr[i] >= stack[stack.length - 1]?.val) stack.pop()
+        stack.push({ val: arr[i], i })
     }
-    let result = [max.val]
-    for (let i = W; i < arr.length; i++) {
-        console.log("begin:", i)
-        if (i - W >= max.i) {
-            if (arr[i] > nextMax.val) {
-                max.val = arr[i]
-                max.i = i
-                nextMax.val = -1 / 0
-            } else {
-                max.val = nextMax.val
-                max.i = nextMax.i
-                nextMax.val = -1 / 0
-                for (let j = nextMax.i + 1; j <= i; j++) {
-                    if (arr[j] > nextMax.val) {
-                        nextMax.val = arr[j]
-                        nextMax.i = j
-                    }
-                }
-                // nextMax.val = arr[i]
-                // nextMax.i = i
-            }
-            result.push(max.val)
-            continue
-        }
+    // initialize result array
+    let result = [stack[0].val]
 
-        if (arr[i] > max.val) {
-            max.val = arr[i]
-            max.i = i
-            nextMax.val = -1 / 0
-        } else if (arr[i] > nextMax.val) {
-            nextMax.val = arr[i]
-            nextMax.i = i
-        }
-        result.push(max.val)
+    // loop through remainder of array to complete problem
+    for (let i = W; i < arr.length; i++) {
 
     }
     return result
 }
 
-const arr = [5, 4, 3, 2, 1]
-const W = 3
+const arr = [5, 4, 3, 6, 4, 7, 8, 1, 4, 3, 2, 0]
+const W = 5
 const result = slidingWindowMax(arr, W)
 console.log(result)
