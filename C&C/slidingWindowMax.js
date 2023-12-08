5a038e62f81e1abc4b37468a9062c2af1d07b41e
davebro1031 <dwbroaddus@gmail.com>
2023-12-08 01:15:35 -0700

    fix broken soln

--- C&C/slidingWindowMax.js
+++ C&C/slidingWindowMax.js
@@ -21,6 +21,7 @@ const slidingWindowMax = (arr, W) => {
     }
     let result = [max.val]
     for (let i = W; i < arr.length; i++) {
+        console.log("begin:", i)
         if (i - W >= max.i) {
             if (arr[i] > nextMax.val) {
                 max.val = arr[i]
@@ -29,8 +30,15 @@ const slidingWindowMax = (arr, W) => {
             } else {
                 max.val = nextMax.val
                 max.i = nextMax.i
-                nextMax.val = arr[i]
-                nextMax.i = i
+                nextMax.val = -1 / 0
+                for (let j = nextMax.i + 1; j <= i; j++) {
+                    if (arr[j] > nextMax.val) {
+                        nextMax.val = arr[j]
+                        nextMax.i = j
+                    }
+                }
+                // nextMax.val = arr[i]
+                // nextMax.i = i
             }
             result.push(max.val)
             continue
@@ -50,7 +58,7 @@ const slidingWindowMax = (arr, W) => {
     return result
 }
 
-const arr = [6, 1, 9, 3, 1, 7, 2, 4, 5, 2]
-const W = 5
+const arr = [5, 4, 3, 2, 1]
+const W = 3
 const result = slidingWindowMax(arr, W)
 console.log(result)
