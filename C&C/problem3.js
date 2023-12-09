00ff9be2d6d51962a82070be257b9d1b61eab65c
davebro1031 <dwbroaddus@gmail.com>
2023-12-09 16:34:55 -0700

    unfinish p3

---
+++ C&C/problem3.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var minimumCoins = function(prices) {
+    let totalCost = prices[0]
+    for(let pi = 1; pi <= prices.length; pi++){
+        let i = pi-1
+
+    }
+};
+
+const prices = [1,10,1,1]
+const result = minimumCoins(prices)
+console.log(result)
