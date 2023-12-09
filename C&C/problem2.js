fc9cdec4c4acf1339c34f3118cfae5b68d84fe95
davebro1031 <dwbroaddus@gmail.com>
2023-12-08 20:02:48 -0700

    remove bad soln

--- C&C/problem2.js
+++ C&C/problem2.js
@@ -6,22 +6,7 @@
  * @return {number}
  */
 var maximizeSquareHoleArea = function(n, m, hBars, vBars) {
-    let remH = []
-    let remV = []
-    for(let i = 1; i <= n + 2; i++){
-        if(hBars.includes(i)) continue
-        remH.push(i)
-    }
-    for(let i = 1; i <= m+2; i++){
-        if(vBars.includes(i)) continue
-        remV.push(i)
-    }
-    let hMax = 1
-    for(let i = 1; i < remH.length; i++) hMax = Math.max(hMax, remH[i]-remH[i-1])
-
-    let vMax = 1
-    for(let i = 1; i < remV.length; i++) vMax = Math.max(vMax, remV[i] - remV[i-1])
-    return Math.min(hMax, vMax)**2
+    
 };
 
 const n = 5, m = 7, hBars = [2,3,4,6], vBars = [2,4,7,8]
