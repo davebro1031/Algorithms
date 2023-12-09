4e9a98e7b4ba50f645dd9fc48b893d8c3c3c350d
davebro1031 <dwbroaddus@gmail.com>
2023-12-08 19:56:41 -0700

    complete prob2 draft

--- C&C/problem2.js
+++ C&C/problem2.js
@@ -16,10 +16,17 @@ var maximizeSquareHoleArea = function(n, m, hBars, vBars) {
         if(vBars.includes(i)) continue
         remV.push(i)
     }
-
-    return 
+    let hMax = 1
+    for(let i = 1; i < remH.length; i++){
+        hMax = Math.max(hMax, remH[i]-remH[i-1])
+    }
+    let vMax = 1
+    for(let i = 1; i < remV.length; i++) {
+        vMax = Math.max(vMax, remV[i] - remV[i-1])
+    }
+    return Math.min(hMax, vMax)**2
 };
 
-const n = 2, m = 3, hBars = [2,3], vBars = [3,4]
+const n = 2, m = 3, hBars = [2,3], vBars = [2,3,4]
 const result = maximizeSquareHoleArea(n, m, hBars, vBars)
 console.log(result)
