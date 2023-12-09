4235648bbf9366cc13d12c58c8a9b4cd4a487881
davebro1031 <dwbroaddus@gmail.com>
2023-12-08 19:34:25 -0700

    begin p1

---
+++ C&C/problem1.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+    
+};
+
+const words = ["abc","bcd","aaaa","cbc"], x = "z"
+const result = findWordsContaining(words, x)
+console.log(result)
