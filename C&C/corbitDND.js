3790553a0313f2fbb73e45f1ae18ab989b0c8d4a
davebro1031 <dwbroaddus@gmail.com>
2023-12-13 15:53:28 -0700

    init corbit problem

---
+++ C&C/corbitDND.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 6; i++) {
+    for (let j = 1; j <= 6; j++) {
+        for (let k = 1; k <= 6; k++) {
+            for (let l = 1; l <= 6; l++) {
+
+            }
+        }
+    }
+}
