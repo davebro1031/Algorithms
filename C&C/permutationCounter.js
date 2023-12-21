b4f4210f3015cdfaf5de4bc4c807872d0ddd4ca1
davebro1031 <dwbroaddus@gmail.com>
2023-12-21 11:59:20 -0700

    add driver

--- C&C/permutationCounter.js
+++ C&C/permutationCounter.js
@@ -1,4 +1,11 @@
 
 const findPermsLen5 = (str) => {
-
+    let perms = []
+    for(let i of str){
+        console.log(i)
+    }
 }
+
+const str = "ADDITION"
+const result = findPermsLen5(str)
+console.log(result)
