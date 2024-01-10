e0f81030dabd97e027f11487eb8cccac60c55b05
davebro1031 <dwbroaddus@gmail.com>
2024-01-10 16:05:14 -0700

    add first pass at heapsort

---
+++ heapSort.js
@@ -0,0 +1,31 @@
+const left = (i) => {
+    return 2 * (i + 1) - 1
+}
+const right = (i) => {
+    return 2 * (i + 1)
+}
+const parent = (i) => {
+    return Math.floor((i + 1) / 2) - 1
+}
+
+
+
+const heapSize = 10
+
+const maxHeapify = (arr, i) => {
+    let l = left(i)
+    let r = right(i)
+    let largest = i
+    if (l <= heapSize && arr[i] < arr[l]) largest = l
+    if (r <= heapSize && arr[largest] < arr[r]) largest = r
+    if (largest === i) return
+    let key = arr[i]
+    arr[i] = arr[largest]
+    arr[largest] = key
+    maxHeapify(arr, largest)
+}
+
+const heap = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
+
+maxHeapify(heap, heapSize)
+console.log(heap)
