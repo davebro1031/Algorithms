8af3a79c88854d0525123cbd78a501180d1fe41e
davebro1031 <dwbroaddus@gmail.com>
2024-01-10 12:21:25 -0700

    add todo

--- Combinatorics/snakeCube.js
+++ Combinatorics/snakeCube.js
@@ -3,5 +3,10 @@ let b1 = "esb sbs bsb sbs bbb bbb bbs bbb bbe"
 let a2 = [2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 let a3 = [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2]
 
+// write converter between 1,2 representation and esb representation
+// write width finder
+// write algorithm that checks if it is a cube or not.... (scary)
+// writh algorithm from another direction? start with the cube and find a path through it?
+
 console.log(a3.reduce((acc, cur) => acc + cur))
 
