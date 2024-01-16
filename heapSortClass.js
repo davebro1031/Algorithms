fa1c617ecb156e5d7dee718a056b18d5513b1527
davebro1031 <dwbroaddus@gmail.com>
2024-01-16 13:45:34 -0700

    add first heapsort

--- heapSortClass.js
+++ heapSortClass.js
@@ -4,15 +4,15 @@ class maxHeap {
         this.heapsize = 0
     }
     get length() { return this.heap.length }
-    left(i) { return 2 * (i + 1) - 1 }
-    right(i) { return 2 * (i + 1) }
+    left(i) { return 2 * i + 1 }
+    right(i) { return 2 * i + 2 }
     parent(i) { return Math.floor((i + 1) / 2) - 1 }
     heapify(i) {
         let j = i
         let l = this.left(j)
         let r = this.right(j)
         let largest = j
-        if (l <= this.heapsize && this.heap[l] > this.heap[j]) largest = j
+        if (l <= this.heapsize && this.heap[l] > this.heap[j]) largest = l
         if (r <= this.heapsize && this.heap[r] > this.heap[largest]) largest = r
         while (j < largest) {
 
@@ -24,17 +24,32 @@ class maxHeap {
 
             let l = this.left(j)
             let r = this.right(j)
-            if (l <= this.heapsize && this.heap[l] > this.heap[j]) largest = j
+            if (l <= this.heapsize && this.heap[l] > this.heap[j]) largest = l
             if (r <= this.heapsize && this.heap[r] > this.heap[largest]) largest = r
         }
     }
     build() {
-        for (i = Math.floor(this.length / 2) - 1; i >= 0; i--) this.heapify(i)
+        this.heapsize = this.length - 1
+        for (let i = Math.floor(this.length / 2) - 1; i >= 0; i--) this.heapify(i)
     }
-
 }
 
-var heap = new maxHeap([1, 5, 9, 2, 7, 4])
-heap.heapsize = 100
-heap.heapify(3)
+const heapSort = (a) => {
+    a.build()
+    console.log(a)
+    for (let i = a.length - 1; i >= 1; i--) {
+        let key = a.heap[0]
+        a.heap[0] = a.heap[i]
+        a.heap[i] = key
+
+        a.heapsize--
+        console.log(a)
+
+        a.heapify(0)
+        console.log(a)
+    }
+}
+
+var heap = new maxHeap([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
+heapSort(heap)
 console.log(heap)
